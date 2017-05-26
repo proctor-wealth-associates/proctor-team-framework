a1785478afd6ecbf42886d17a54c6e4b64fa07b1
Loris LEIVA <loris.leiva@gmail.com>
2017-05-26 15:59:30 +0200

    :recycle: Remove tedious native documentation

--- src/Foundation/Stubs/app/Http/Controllers/Auth/LoginController.php
+++ src/Foundation/Stubs/app/Http/Controllers/Auth/LoginController.php
@@ -7,30 +7,15 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
     use AuthenticatesUsers;
 
     /**
      * Where to redirect users after login.
-     *
-     * @var string
      */
     protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
-     *
-     * @return void
      */
     public function __construct()
     {
@@ -39,8 +24,6 @@ class LoginController extends Controller
 
     /**
      * Show the application's login form.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function showLoginForm()
     {
--- src/Teams/Stubs/app/Http/Controllers/Auth/RegisterController.php
+++ src/Teams/Stubs/app/Http/Controllers/Auth/RegisterController.php
@@ -11,30 +11,15 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
-
     use RegistersUsers;
 
     /**
      * Where to redirect users after registration.
-     *
-     * @var string
      */
     protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
-     *
-     * @return void
      */
     public function __construct()
     {
@@ -43,8 +28,6 @@ class RegisterController extends Controller
 
     /**
      * Show the application registration form.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function showRegistrationForm()
     {
@@ -55,9 +38,6 @@ class RegisterController extends Controller
 
     /**
      * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
      */
     protected function validator(array $data)
     {
@@ -70,9 +50,6 @@ class RegisterController extends Controller
 
     /**
      * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return User
      */
     protected function create(array $data)
     {
@@ -85,10 +62,6 @@ class RegisterController extends Controller
 
     /**
      * The user has been registered.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  mixed  $user
-     * @return mixed
      */
     protected function registered(Request $request, $user)
     {
